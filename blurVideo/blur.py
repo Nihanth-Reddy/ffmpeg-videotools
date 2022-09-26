35869c436fe7660dec77897f5f70a64811165af4
Nihanth <writetonihanth@gmail.com>
2022-09-27 03:42:18 +0530

    updated file_path parameter

--- blurVideo/blur.py
+++ blurVideo/blur.py
@@ -1,10 +1,20 @@
 import subprocess
+import time
 
-def blur_video(timestamps):
+def blur_video(timestamps: list, file_path: str):
+    """
+    Args:
+        timestamps: List of video segment timestamps which needs to be blurred
+        file_path: Path of the input video
+    """
     query = ""
+    
+    # Generates the query by iterating through the list of timestamps
     for index, timestamp in enumerate(timestamps):
         if index == 0:
             query = f"[0:v]boxblur=30:enable='between(t,{timestamp[0]},{timestamp[1]})' [fg{index}];"
         else:
             query = query +  f"[fg{index-1}]boxblur=30:enable='between(t,{timestamp[0]},{timestamp[1]})' [fg{index}];"
-    subprocess.run(["ffmpeg", "-y", "-i", ".\\blurVideo\\files\\input.mp4", "-filter_complex", query[:-1], "-map", f"[fg{len(timestamps)-1}]", ".\\blurVideo\\files\\blurred.mp4"])
+    
+    # Runs the FFMPEG command with filter query generated
+    subprocess.run(["ffmpeg", "-y", "-i", file_path, "-filter_complex", query[:-1], "-map", f"[fg{len(timestamps)-1}]", f".\\blurVideo\\files\\blurred{int(time.time())}.mp4"])
--- blurVideo/tasks.py
+++ blurVideo/tasks.py
@@ -8,10 +8,9 @@ from .email import send_email
 logger = get_task_logger(__name__)
 
 @shared_task(name="blur_video")
-def blur_video_task(timestamps: list, email: str):
+def blur_video_task(timestamps: list, file_path:str, email: str):
     logger.info(f"List of timestamps are: {timestamps}")
-    blur_video(timestamps)
+    blur_video(timestamps, file_path)
     logger.info("Video processing is completed. Initiating email notification")
     send_email(email)
     logger.info("Email notification is sent")
-
